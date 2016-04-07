a08a3ca71e37efcaacbc96c11ab9c1fe5a3a7583
Chris MacNaughton <chmacnaughton@gmail.com>
2016-04-07 10:10:23 -0400

    initial commit

---
+++ reactive/ceph_base.py
@@ -0,0 +1,17 @@
+from charms.reactive import when, when_not, set_state
+
+
+@when_not('ceph-base.installed')
+def install_ceph-base():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_state('ceph-base.installed')
